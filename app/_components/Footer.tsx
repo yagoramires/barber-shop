a84ada922d28ae1a37fc79779521b613987b6702
yagoramires <yago.ramiresx@gmail.com>
2024-01-30 12:15:59 -0300

    feat: add footer component

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="w-full bg-secondary px-5 py-6 text-center">
-      <p className="text-gray-400">&copy; Created by Yago Ramires</p>
+      <p className="text-sm text-gray-400">
+        &copy; 2023 Copyright{" "}
+        <span className="font-semibold">Yago Ramires</span>
+      </p>
     </footer>
   );
 }
