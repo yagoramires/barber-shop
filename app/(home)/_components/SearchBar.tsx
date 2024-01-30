2131bf35005067d8b988983dbd67c18ee67b8d0f
yagoramires <yago.ramiresx@gmail.com>
2024-01-30 19:23:01 -0300

    styles: adjusting layout based on figma

--- app/(home)/_components/SearchBar.tsx
+++ app/(home)/_components/SearchBar.tsx
@@ -6,8 +6,11 @@ import { SearchIcon } from "lucide-react";
 export default function SearchBar() {
   return (
     <div className="flex items-center gap-2">
-      <Input className="focus-visible:ring-transparent" />
-      <Button size="icon" variant="default">
+      <Input
+        className="focus-visible:ring-transparent"
+        placeholder="Encontre seu barbeiro"
+      />
+      <Button size="icon" variant="default" className="p-[10px]">
         <SearchIcon size={18} />
       </Button>
     </div>
