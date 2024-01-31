7f009c2be04b59daa38160c2b7fcc279077a1f2a
yagoramires <yago.ramiresx@gmail.com>
2024-01-30 21:45:01 -0300

    feat: add next-auth setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import { db } from "@/app/_lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
