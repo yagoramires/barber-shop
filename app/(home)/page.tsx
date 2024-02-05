7d9f6cbacfc474026c365ed3aebff19ae2fb5b56
yagoramires <yago.ramiresx@gmail.com>
2024-02-04 22:01:03 -0300

    fix: adjust header import

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,11 +5,11 @@ import { Barbershop, Booking as IBooking } from "@prisma/client";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
-import Header from "@/app/_components/Header";
 import SearchBar from "./_components/SearchBar";
 import BookingCard from "@/app/_components/BookingCard";
 import BarberShopCard from "./_components/BarberShopCard";
 import { authOptions } from "../_lib/auth";
+import Header from "../_components/Header";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
